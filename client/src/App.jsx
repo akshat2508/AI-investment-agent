c80700484a07ba3073ebffac734f4095452c260e
Akshat-paul <akshatpaul2006@gmail.com>
2026-07-04 18:45:57 +0530

    Inital setup with react and express

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl font-bold">
+      AI Investment Research Agent
+    </div>
+  );
+}
+
+export default App;
